9005720c4972d4a4c9e922593343a0e72cdc0e3e
Ryuki Tanaka <hdhvutcniy@gmail.com>
2023-10-25 15:15:55 +0900

    build CI/CD pipeline with GitHub Actions02

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   # Assumes s3 bucket and dynamo DB table already set up
   backend "s3" {
-    bucket         = "aws-ecs-terraform-tfstate-iefnaiof3"
+    bucket         = "aws-ecs-terraform-tfstate-iefnaiof32432"
     key            = "terraform.tfstate"
     region         = "ap-northeast-1"
     dynamodb_table = "aws-ecs-terraform-tfstate-locking"
